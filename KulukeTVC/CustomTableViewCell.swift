f5f07e925914306a4e17cca1663e1d5e06025a1d
Temirlan <fullbaby605@gmail.com>
2023-02-20 16:28:54 +0600

    Создал новый класс tableViewCell и привязал его к ячейке , для работы с ячейкой

---
+++ KulukeTVC/CustomTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CustomTableViewCell.swift
+//  KulukeTVC
+//
+//  Created by Темирлан Асанбеков on 20/2/23.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
