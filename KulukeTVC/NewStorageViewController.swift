0b9b302b690edbd65deff4995737abfd26330f40
Temirlan <fullbaby605@gmail.com>
2023-02-21 13:39:46 +0600

    прописал в методе делегата  иконки для кнопок камера и фото и разместил название кнопок слева

--- KulukeTVC/NewStorageViewController.swift
+++ KulukeTVC/NewStorageViewController.swift
@@ -23,15 +23,22 @@ class NewStorageViewController: UITableViewController, UINavigationControllerDel
     // данный метод нужен для работы с ячейками где по нажатию клавиатура будет скрываться относительно логики метода
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
+        let cameraIcon = UIImage(imageLiteralResourceName: "camera")
+        let photoIcon = UIImage(imageLiteralResourceName: "photo")
+        
         if indexPath.row == 0 {
             let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
             let camera = UIAlertAction(title: "Camera", style: .default) { _ in
                 self.chooseImagePicker(source: .camera)
             }
+            camera.setValue(cameraIcon, forKey: "image")
+            camera.setValue(CATextLayerAlignmentMode.left, forKey: "titleTextAlignment")
             
             let photo = UIAlertAction(title: "Photo", style: .default) { _ in
                 self.chooseImagePicker(source: .photoLibrary)
             }
+            photo.setValue(photoIcon, forKey: "image")
+            photo.setValue(CATextLayerAlignmentMode.left, forKey: "titleTextAlignment")
             
             let cancel = UIAlertAction(title: "Cancel", style: .cancel)
             
